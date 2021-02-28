6b261696263a1a1211788a554e3fb65012cc948c
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-02-28 21:34:35 +0530

    user created, db migration

--- ajaxApp/admin.py
+++ ajaxApp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Employee, Office
 # Register your models here.
+
+class EmployeeInline(admin.TabularInline):
+    model = Employee
+
+
+@admin.register(Office)
+class OfficeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location',)
+    list_display_links = ('name', 'location',)
+    inlines = [ EmployeeInline ]
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'gender', 'office')
+    list_display_links = ('first_name', 'last_name',
+                          'email', 'gender', 'office')
+    
--- ajaxApp/models.py
+++ ajaxApp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Office(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Employee(models.Model):
+    GENDER = (
+        ('female', "Female"),
+        ('male', "Male"),
+    )
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254)
+    gender = models.CharField(max_length=50, choices=GENDER)
+    office = models.ForeignKey("ajaxApp.Office", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
